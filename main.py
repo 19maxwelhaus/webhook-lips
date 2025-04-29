ae6cc9cfc9a79e0c972c644bbf4301afd038e4bf
19maxwelhaus <maks192000@gmail.com>
2025-04-29 20:17:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
+# 🔐 Заміни ці дані на свої
 BOT_TOKEN = '7944590947:AAGzJ9xsQVeiAnAcpSy8rOSx5SgCtB8Fk-Q'
 CHAT_ID = '383196764'
 
@@ -11,10 +13,13 @@ def webhook():
     data = request.json
     print("Прийнято:", data)
 
+    # Отримуємо Instagram username з SendPulse
     username = data.get('username', 'невідомо')
 
+    # Формуємо повідомлення
     message = f"🔔 Оплата підтверджена!\nКлієнт: https://instagram.com/{username}\nПеревір вручну."
 
+    # Надсилаємо в Telegram
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         'chat_id': CHAT_ID,
@@ -26,4 +31,5 @@ def webhook():
     return 'ok', 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # ключовий рядок для Render
+    app.run(host='0.0.0.0', port=port)
